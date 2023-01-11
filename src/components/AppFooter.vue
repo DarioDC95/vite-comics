ae931ab9a08bf181a1d74946c3d107ee522b1da7
DarioDC95 <dario_95@live.com>
2023-01-11 22:13:49 +0100

    Last section of the footer remaining

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -46,18 +46,146 @@
                 </div>
             </div>
         </section>
-        <section class="">
-            
+        <section class="footer-links">
+            <div class="mycontainer">
+                <div class="myrow">
+                    <div class="mycol-lg">
+                        <div class="mycard">
+                            <div class="margin-right-xs">
+                                <div class="dc-comics">
+                                    <div class="title">DC COMICS</div>
+                                    <ul>
+                                        <li>
+                                            <a class="underline-none" href="#">Characters</a>
+                                        </li>
+                                        <li>
+                                            <a class="underline-none" href="#">Comics</a>
+                                        </li>
+                                        <li>
+                                            <a class="underline-none" href="#">Movies</a>
+                                        </li>
+                                        <li>
+                                            <a class="underline-none" href="#">TV</a>
+                                        </li>
+                                        <li>
+                                            <a class="underline-none" href="#">Games</a>
+                                        </li>
+                                        <li>
+                                            <a class="underline-none" href="#">Videos</a>
+                                        </li>
+                                        <li>
+                                            <a class="underline-none" href="#">News</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <div class="title">SHOP</div>
+                                    <ul>
+                                        <li>
+                                            <a class="underline-none" href="#">Shop DC</a>
+                                        </li>
+                                        <li>
+                                            <a class="underline-none" href="#">Shop DC Collectibles</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="margin-right-xs">
+                                <div class="title">DC</div>
+                                <ul>
+                                    <li>
+                                        <a class="underline-none" href="#">Terms Of Use</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">Privacy policy (New)</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">Ad Choices</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">Advertising</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">Jobs</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">Subscriptions</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">Talent Workshops</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">CPSC Certificates</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">Ratings</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">Shop Help</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">Contact Us</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <div class="title">SITES</div>
+                                <ul>
+                                    <li>
+                                        <a class="underline-none" href="#">DC</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">MAD Magazine</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">DC Kids</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">DC Universe</a>
+                                    </li>
+                                    <li>
+                                        <a class="underline-none" href="#">DC Power Visa</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mycol-lg">
+                        <div class="mycard">
+                            <img src="../assets/img/dc-logo-bg.png" alt="Logo">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="sign-up">
+            <div class="mycontainer">
+                <div class="myrow">
+                    <div class="mycol">
+                        <div class="mycard">
+                            <button>SIGN-UP NOW!</button>
+                        </div>
+                    </div>
+                    <div class="mycol">
+                        <div class="mycard">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     </footer>
 </template>
 
 <style scoped lang="scss">
 @use '../styles/partials/mixins'as *;
+@use '../styles/partials/variables' as *;
 
 .icon-services {
-    background-color: #0282f9;
+    background-color: $bg-icon-services;
     padding: 50px 0;
+    position: relative;
+    z-index: 5;
 
     .mycontainer {
         @include container-mix;
@@ -79,7 +207,7 @@
 
                         span {
                             font-size: 14px;
-                            color: white;
+                            color: $color-white;
                         }
                     }
                     
@@ -91,5 +219,78 @@
             }
         }
     }
+}
+
+.footer-links {
+    height: 400px;
+    padding-top: 50px;
+    background-image: url('../assets/img/footer-bg.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    .mycontainer {
+        @include container-mix;
+
+        .myrow {
+            display: flex;
+            justify-content: space-between;
+
+            .mycol-lg {
+                width: 50%;
+                position: relative;
+                
+                .mycard{
+                    display: flex;
+                    justify-content: flex-start;
+
+                    img {
+                        position: absolute;
+                        top: -135px;
+                        right: 0;
+                        z-index: 1;
+                    }
+
+                    .dc-comics {
+                        margin-bottom: 20px
+                    }
+
+                    .title {
+                        color: $color-white;
+                        font-size: 20px;
+                        font-weight: 500;
+                        margin-bottom: 15px;
+                    }
+
+                    ul {
+                        list-style-type: none;
+
+                        li {
+                            margin-bottom: 3px;
+
+                            a {
+                                color: $color-grey;
+                                font-size: 13px;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }    
+}
+
+.sign-up {
+    padding: 50px 0;
+    background-color: $bg-brown;
+    position: relative;
+    z-index: 5;
+
+    .mycontainer {
+        @include container-mix;
+
+        .myrow {
+            
+        }
+    } 
 }
 </style>
