33b59be51cdecef1cf26d8da097af023dd9ff6cb
DarioDC95 <dario_95@live.com>
2023-01-12 17:11:45 +0100

    Exercise Done

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -1,35 +1,118 @@
 <script>
+    import AppComicsCards from './AppComicsCards.vue';
+    import dcComics from '../data/dc-comics';
     export default {
         name: 'AppBody',
+        components: {
+            AppComicsCards,
+            dcComics
+        },
         data() {
             return {
+                dcComicsCards: dcComics,
                 message: '-->Content goes here<--'
             }
-        }
+        },
     }
 </script>
 
 <template>
     <main>
-        <div class="mycontainer">
-            {{ message }}
-        </div>
+        <section class="jumbotron">
+            <div class="mycontainer">
+                <div class="current-series">CURRENT SERIES</div>
+            </div>
+        </section>
+        <section class="main-content">
+            <div class="mycontainer">
+                <div class="myrow">
+                    <div v-for="(value, index) in dcComicsCards.dcComics" :key="index" class="mycol-sm">
+                        <AppComicsCards :image="value.thumb" :serie="value.series"/>
+                    </div>
+                </div>
+                <div class="button">
+                    <button>LOAD MORE</button>
+                </div>
+            </div>
+        </section>
     </main>
 </template>
 
 <style scoped lang="scss">
-    @use '../styles/partials/mixins'as *;
+    @use '../styles/partials/mixins' as *;
     @use '../styles/partials/variables' as *;
 
     main {
-        font-size: 20px;
         font-weight: 600;
         color: white;
         background-color: $bg-black;
-        padding: 60px 0;
 
-        .mycontainer {
-            @include container-mix;
+        .jumbotron {
+            height: 400px;
+            background-image: url('../assets/img/jumbotron.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+
+            .mycontainer {
+                @include container-mix;
+                height: 100%;
+                position: relative;
+
+                .current-series {
+                    background-color: $bg-icon-services;
+                    display: inline-block;
+                    padding: 10px 30px;
+                    font-size: 20px;
+                    position: absolute;
+                    bottom: 0;
+                    transform: translateY(50%);
+                }
+            }
+        }
+
+        .main-content {
+            padding-top: 50px;
+            padding-bottom: 20px;
+
+            .mycontainer {
+                @include container-mix;
+                padding: 0 20px;
+
+                .myrow {
+                    display: flex;
+                    justify-content: space-between;
+                    flex-wrap: wrap;
+
+                    .mycol-sm {
+                        width: calc((100% - 120px) / 6);
+                    }
+                }
+
+                .button {
+                    text-align: center;
+
+                    button {
+                        padding: 15px 20px ;
+                        border: 3px solid $color-blue;
+                        background-color: transparent;
+                        font-weight: 600;
+                        font-size: 17px;
+                        color: $color-white;
+                        cursor: pointer;
+        
+                        &:hover {
+                            border: 3px solid $color-white;
+                            color: $color-blue;
+                        }
+        
+                        &:active {
+                            color: $color-white;
+                            background-color: $bg-black;
+                            border: 3px solid $color-blue;
+                        }
+                    }
+                }
+            }
         }
     }
 </style>
---
+++ src/components/AppComicsCards.vue
@@ -0,0 +1,30 @@
+<script>
+    export default {
+        name: 'AppComicsCards',
+        props: {
+            image: String,
+            cost: String,
+            serie: String,
+            categorie: String
+        },
+    }
+</script>
+
+<template>
+    <div class="mycard">
+        <img :src="image" :alt="serie">
+        <h4>{{ serie }}</h4>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .mycard {
+        width: 100%;
+        margin-bottom: 40px;
+        text-transform: uppercase;
+
+        img {
+            margin-bottom: 20px;
+        };
+    }
+</style>
